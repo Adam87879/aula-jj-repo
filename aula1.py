d6b018834f1587f74e414d217b132fccec5ac27b
Adam87879 <adampsilva10@gmail.com>
2024-06-13 20:44:23 -0300

    PAULAÉ10

---
+++ aula1.py
@@ -0,0 +1,8 @@
+#Olá, PAULA MARTINS. Em JANEIRO você realizou uma compra no valor de R$500,00 e ganhou um desconto de 10% em sua próxima compra. Use o cupom PAULAÉ10.
+nome = "Paula Martins "
+mes= "Janeiro"
+valor = 500
+desconto = 10
+cupom = "PAULAÉ10"
+
+print ("Ola " + nome + ". Em " + mes + " voce realizou uma compra no valor de R$" , valor , " e ganhou um desconto de " , desconto , "% em sua proxima compra. Use o cumpom" , cupom ,".")
