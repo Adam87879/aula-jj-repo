192c202abf77a7aff10a58fbdd6f50dac8808fb8
Adam87879 <adampsilva10@gmail.com>
2024-06-13 21:06:27 -0300

    adicionando os tipos de type

---
+++ aula1_type.py
@@ -0,0 +1,9 @@
+tipo_nome = type(nome)
+tipo_valor = type(valor)
+tipo_desconto = type(desconto)
+tipo_cupom = type(cupom)
+
+print(tipo_nome)
+print (tipo_valor)
+print(tipo_desconto)
+print (tipo_cupom)
