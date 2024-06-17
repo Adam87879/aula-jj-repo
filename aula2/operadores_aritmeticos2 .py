b3f3bb82eb94cf655596ba8429a5eb9dce07d19d
Adam87879 <adampsilva10@gmail.com>
2024-06-17 20:52:58 -0300

    pratica de operadores aritmeticos pt2

---
+++ aula2/operadores_aritmeticos2 .py
@@ -0,0 +1,14 @@
+numero = int(input('Digite o nome a ser calculado: '))
+
+n1 = numero * 2
+n2 = numero * 3
+n3 = numero ** 2
+n4 = numero ** (1/2)
+n5 = numero ** (1/3)
+
+
+print(f"O dobro de {numero} é {n1}")
+print(f"O triplo de {numero} é {n2}")
+print(f"O quadrado de {numero} é {n3}")
+print(f"A raiz quadrada de {numero} é {n4}")
+print(f"A raiz cúbica de {numero} é {n5}")
